827838bad502c3ff71c39fe3e928371cc933efa4
lawkeen <46008086+lawkeen@users.noreply.github.com>
2018-12-27 14:37:41 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,24 +1,23 @@
 #include "stdafx.h"
-#include "Cheking.h"
+#include "Cheсking.h"
 #include "DisplayReadElements.h"
 #include "SudokuSolverGenerator.h"
 
-
-int a[9][9], b[9][9], r[60][2];
+int a[9][9], b[9][9], r[81][2];
 int row, col, icount, mode;
 char solh;
 
-
-
 int main()
 {
-	SolveGenerate z;
-	DisplayAndRead z1;
+	Solve z;
+	Generate z1;
+	Read z2;
+	Display z3;
 	int i, j, error;
 	char c;
 	system("cls");
 	printf("\n\n\n\n\t\tEnter '1' to GENERATE SuDoKu Puzzle\n\n\t\tEnter any other key to SOLVE SuDoKu\n\n\n\n\t\tEnter: ");
-	scanf("%c", &c);
+	scanf_s("%c", &c);
 	system("cls");	
 	printf("\n\n\t\t\tHOW TO ENTER ELEMENTS\n");
 	printf("\n\n1.Enter the elements by specifying the row and column number of\n  corresponding location");
@@ -30,14 +29,17 @@ int main()
 	if (c == '1')
 	{		
 		getchar();	
-		z.generate();
-		if (solh == 'y' || solh == 'Y')
-			z1.readvalues();
+		z1.generate();
+		if (solh == 'y' || solh == 'Y')			z2.readvalues();
 		for (i = 0; i < 9; i++)
+		{
 			for (j = 0; j < 9; j++)
+			{
 				a[i][j] = b[i][j];
+			}
+		}
 		system("cls");
-		z1.display();
+		z3.display();
 		getchar();
 		getchar();
 		exit(0);
@@ -46,21 +48,25 @@ read:
 	icount = 0;
 	row = 0; col = 0;
 	for (i = 0; i < 9; i++)
+	{
 		for (j = 0; j < 9; j++)
+		{
 			a[i][j] = 0;
-	for (i = 0; i < 60; i++)
+		}
+	}
+	for (i = 0; i < 81; i++)
 	{
 		r[i][0] = 20; r[i][1] = 20;
 	}
 	system("cls");
 	printf("\n\nNow you can type some modificators you want to programme solve ");
-	z1.readvalues();
+	z2.readvalues();
 	getchar();
 	system("cls");
 ask:
-	z1.display();
-	printf("\n\nEnter 'm' to MODIFY\nEnter 'r' to RE-ENTER\nEnter any other key for SUDOKU Solution: ");
-	scanf("%c", &c);
+	z3.display();
+	printf("\n\nEnter 'm' to MODIFY\nEnter 'r' to RE-ENTER\nEnter'ENTER' for SUDOKU Solution: ");
+	scanf_s("%c", &c);
 	if (c == 'r' || c == 'R')
 	{
 		system("cls");
@@ -69,7 +75,7 @@ ask:
 	if (c == 'm' || c == 'M')
 	{
 		printf("\n\n");
-		z1.readvalues();
+		z2.readvalues();
 		system("cls");
 		getchar();
 		goto ask;
@@ -77,11 +83,10 @@ ask:
 next:
 	error = z.solve();
 	system("cls");
-	z1.display();
-	if (error == 0)
-		printf("\n\n\n\n\rNO SOLUTION or TOO MANY MODIFICATIONS");
-	printf("\n\n\n\n\n\rEnter z to continue and any other key to exit: ");
-	scanf("%c", &c);
+	z3.display();
+	if (error == 0)		printf("\n\n\n\n\rNO SOLUTION or TOO MANY MODIFICATIONS");
+	printf("\n\n\n\n\n\rEnter c to continue and any other key to exit: ");
+	scanf_s("%c", &c);
 	if (c == 'c' || c == 'C')
 	{
 		goto read;
