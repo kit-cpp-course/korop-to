a8f9a225ef703d4ad6e65c0da62ce51723285579
lawkeen <46008086+lawkeen@users.noreply.github.com>
2018-12-28 10:08:26 +0300

    Update SudokuSolverGenerator.h

--- SudokuSolverGenerator.h
+++ SudokuSolverGenerator.h
@@ -7,6 +7,10 @@ class Solve
 public:
 	/*function to solve sudoku*/
 	int solve();
+	/*helful methods needed for solve()*/
+	int mainallot();
+
+	int backadjust();
 };
 
 //class with fucntion needed to generate sudoku
@@ -15,4 +19,8 @@ class Generate
 public:
 	/*function to solve sudoku*/
 	void generate();
+	/*helful methods needed for generate()*/
+	void gen();
+
+	void allot();
 };
